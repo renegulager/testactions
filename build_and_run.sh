85c818ec3c568ec888e64298e4b63ca94aa41574
Rene Gulager <mail@renegulager.dk>
2022-09-07 09:17:32 +0200

    aa8

--- build_and_run.sh
+++ build_and_run.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 # get file path
 cwd=`dirname "${0}"`
-name=`git describe --tags`
+name=$( git describe --tags )
 expr "${0}" : "/.*" > /dev/null || cwd=`(cd "${cwd}" && pwd)`
-
 gcc ${cwd}/main.cpp -lstdc++ -o bin/$name.out
