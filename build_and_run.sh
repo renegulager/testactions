638b35919be653f968499e42fff16328ec0fa2fa
Rene Gulager <mail@renegulager.dk>
2022-09-07 09:45:30 +0200

    g1

--- build_and_run.sh
+++ build_and_run.sh
@@ -1,9 +1,7 @@
 #!/bin/sh
 # get file path
 cwd=`dirname "${0}"`
-name=$( git describe --tags )
-echo "name" $name
 mkdir -p ${cwd}/bin
 expr "${0}" : "/.*" > /dev/null || cwd=`(cd "${cwd}" && pwd)`
-gcc ${cwd}/main.cpp -lstdc++ -o ${cwd}/bin/$name.out
+gcc ${cwd}/main.cpp -lstdc++ -o ${cwd}/bin/bb.out
 ls ${cwd}/bin
