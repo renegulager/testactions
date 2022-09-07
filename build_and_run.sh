1b9f7bff81134099ce5e7ef7d5c0a969d003b636
Rene Gulager <mail@renegulager.dk>
2022-09-07 09:05:25 +0200

    aa5

--- build_and_run.sh
+++ build_and_run.sh
@@ -4,4 +4,4 @@ cwd=`dirname "${0}"`
 name=$1
 expr "${0}" : "/.*" > /dev/null || cwd=`(cd "${cwd}" && pwd)`
 
-gcc ${cwd}/main.cpp -lstdc++ -o bin/$1.out
+gcc ${cwd}/main.cpp -lstdc++ -o bin/$GITHUB_REF_NAMe.out
