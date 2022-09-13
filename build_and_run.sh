705ccd637329be3906913b1997eacac9364347d3
Rene Gulager <mail@renegulager.dk>
2022-09-13 13:53:34 +0200

    add build out

--- build_and_run.sh
+++ build_and_run.sh
@@ -3,5 +3,5 @@
 cwd=`dirname "${0}"`
 mkdir -p ${cwd}/bin
 expr "${0}" : "/.*" > /dev/null || cwd=`(cd "${cwd}" && pwd)`
-gcc ${cwd}/main.cpp -lstdc++ -o ${cwd}/bin/bb.out
+gcc ${cwd}/main.cpp -lstdc++ -o ${cwd}/bin/$1.out
 ls ${cwd}/bin
